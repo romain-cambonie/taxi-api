169783b2095074b93ee2484d132cdbf8db875796
Romain Cambonie <romain.cambonie@gmail.com>
2023-01-26 12:51:43 +0100

    feat: add closeGracefully logic

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,48 @@
 // TODO This is an insecure http server set to http2 before production !
-import fastify from 'fastify'
+import fastify, {FastifyInstance} from 'fastify'
 
-const server = fastify()
+const server: FastifyInstance = fastify();
+const PORT: number = 3001;
+const HOST: string =  "localhost";
 
 server.get('/ping', async (_request, _reply) => {
     return 'pong\n'
 })
 
-server.listen({ port: 3000 }, (err, address) => {
-    if (err) {
-        console.error(err)
+server.get('/delayed', async (_request, _reply) => {
+    const SECONDS_DELAY = 20000;
+    await new Promise<void>(resolve => {
+        setTimeout(() => resolve(), SECONDS_DELAY)
+    })
+    return { hello: 'delayed world' }
+})
+
+async function closeGracefully(signal: string | number | undefined) {
+    console.log(`*^!@4=> Received signal to terminate: ${signal}`)
+
+    await server.close()
+    // await db.close() if we have a db connection in this app
+    // await other things we should cleanup nicely
+    process.kill(process.pid, signal);
+}
+
+process.once('SIGINT', closeGracefully)
+process.once('SIGTERM', closeGracefully)
+
+const start = async () => {
+    try {
+        await server.listen({ port: PORT, host: HOST }, (err, address) => {
+            if (err) {
+                console.error(err)
+                process.exit(1)
+            }
+            console.log(`Server listening at ${address}`)
+        });
+        console.log(`*^!@4=> Process id: ${process.pid}`)
+    } catch (err) {
+        server.log.error(err)
         process.exit(1)
     }
-    console.log(`Server listening at ${address}`)
-})
+}
+
+start();
